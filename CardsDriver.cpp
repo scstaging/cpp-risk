4a7ba56536efef2c9222985932c85781dded1b05
Gabriel DAlesio <indigo4038@gmail.com>
2023-10-05 14:04:58 -0400

    Last lil change for better testing
    Added comments in cardsdriver file and gets size of the deck each time it's changed.

--- CardsDriver.cpp
+++ CardsDriver.cpp
@@ -3,22 +3,32 @@
 #include "Cards.cpp"
 
 int testCards(){
-
+    // Create a new deck
     Deck* testDeck = new Deck();
 
+    // Return the size of the deck. Should be 15. 
     testDeck->getDeck();
 
+    // Creates a new hand 
     Hand* testHand = new Hand();
 
+    // Draw five cards from the deck and place them in the hand
     for(int i = 0; i < 5; i++){
         testDeck->draw(testHand);
     }
 
+    // Return the size of the deck. Should be 10. 
+    testDeck->getDeck();
+
+    // Create a list of cards and assign it to the cards in the hand
     list<Card> cards = *testHand->cardsInHand;
 
+    // Play each card in the hand, depositing them back into the deck
     for(Card c : cards){
         c.play(testDeck, testHand);
     }
-
+    
+    // Return the size of the deck. Should be 15. 
+    testDeck->getDeck();
 
 }
